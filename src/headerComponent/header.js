472bb3552a9d2f346899b5c90e106e8662188dee
praditya <praditya.rendi88@gmail.com>
2018-11-28 14:47:09 +0700

    Latihan merubah react

---
+++ src/headerComponent/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render(){
+        return (
+            <header>
+
+            test Header
+
+            </header>
+        )
+    }
+}
+
+export default Header;
